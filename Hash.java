abc774f9145024f0dd95982a05c1bc96aa062b4d
yamazaki <ma17123@shibaura-it.ac.jp>
2018-01-08 14:32:38 +0900

    ハッシュ実装

---
+++ Hash.java
@@ -0,0 +1,19 @@
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import javax.xml.bind.DatatypeConverter;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Hash {
+    public static String hashGanerate(String source, String algorithm)throws UnsupportedEncodingException, NoSuchAlgorithmException {
+      Charset charset = StandardCharsets.UTF_8;
+      byte[] bytes = MessageDigest.getInstance(algorithm).digest(source.getBytes(charset));
+      String result = DatatypeConverter.printHexBinary(bytes);
+      return result;
+      }
+
+  }
--- MainAES.java
+++ MainAES.java
@@ -5,11 +5,15 @@ import java.util.logging.Logger;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainAES {
   private static final String KEY = "YKo83n14SWf7o8G5";
   private static final String ALGORITHM = "AES";
-    public static void main(String [] args) {
+    public static void main(String [] args) throws UnsupportedEncodingException, NoSuchAlgorithmException {
     try {
       String source = "test";
       String encrypt = CipherManager.encrypt(source, KEY, ALGORITHM);
@@ -17,6 +21,8 @@ public class MainAES {
       System.out.println("Source: "  + source);
       System.out.println("Encrypt: " + encrypt);
       System.out.println("Decrypt: " + decrypt);
+      String hash = Hash.hashGanerate(encrypt, "MD5");
+      System.out.println("hash: " + hash);
     } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
       Logger.getLogger(MainAES.class.getName()).log(Level.SEVERE, null, ex);
     }
